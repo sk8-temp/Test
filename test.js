6a42a74f9e333e28e32b6f353e026c69464b6df2
sk8-temp <sarbajit.230103038@iiitbh.ac.in>
2025-07-31 10:53:07 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+function checkEvenOrOdd(number) {
+  if (number % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+console.log(checkEvenOrOdd(7)); // Output: Odd
+console.log(checkEvenOrOdd(12)); // Output: Even
