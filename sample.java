61ef65a0d292959f384c64c9d5155bcab60c5e3c
sk8-temp <sarbajit.230103038@iiitbh.ac.in>
2025-08-04 21:39:39 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        // This line prints the message to the console
         System.out.println("Hello, World!"); 
     }
 }
